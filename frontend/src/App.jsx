a90fee973756bf2e3689efdaf4797871d56ae720
Anshuman Singh <anshuman.singh08@infosys.com>
2025-04-22 17:39:32 +0530

    Frontend boilercode added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <>Hello Worl trying csomethingd</>;
+}
+
+export default App;
