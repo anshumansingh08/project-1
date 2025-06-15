3a7bcfdb084c3ff51d627bea3eda3fdc4673fc68
anshumansingh08 <anshuman.singh08@infosys.com>
2025-06-15 10:49:59 +0530

    New package installed
    proxy added to Vite config to allow cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,4 +11,12 @@ export default defineConfig({
       jsxRuntime: "automatic",
     }),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
