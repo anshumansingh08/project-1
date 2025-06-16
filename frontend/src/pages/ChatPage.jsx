1cd564f0303b4329536f4988e3e02bfb78560415
anshumansingh08 <anshuman.singh08@infosys.com>
2025-06-16 14:43:45 +0530

    Code changes

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [chat, setChat] = useState([]);
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    console.log(data);
+    setChat(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+  return (
+    <div>
+      <h1>ChatPage</h1>
+      {chat.map((data) => (
+        <div key={data._id}>{data.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
